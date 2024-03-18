be6b382ce3aa6c34a1501ae3426c4b4198739948
Nikita Titarenko <originalfruitybang@gmail.com>
2024-03-18 16:33:15 +0300

    pg connected

--- db.py
+++ db.py
@@ -2,16 +2,30 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import MetaData
 from typing import Optional
+import asyncio
+import aiomysql
 
 
 URL_DATABASE = (
-    'postgresql+asyncpg://postgres:postgres@127.0.0.1:5432/postgres'
+    'postgresql+asyncpg://bobr:Kurwa_Perdole@127.0.0.1:5432/pg_images_db'
 )
+
+#URL_DATABASE = (
+#    'mysql+aiomysql://bobr:Kurwa_Perdole@127.0.0.1:5432/mysql_images_db?charset=utf8mb4'
+#)
 
 engine = create_async_engine(URL_DATABASE, future=True, echo=True)
 # ('sqlite+aiosqlite:///images.db')
 
 db_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+#loop = asyncio.get_event_loop()
+#
+#async def connection():
+#    conn = await aiomysql.connect(host='127.0.0.1', port=5432,
+#                                  user='root', password='Kurwa_Perdole',
+#                                  db='mysql_images_db', loop=loop)
 
 
 class Base(DeclarativeBase):
--- repository.py
+++ repository.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 import sqlalchemy.orm
 from transliterate import translit
 from db import db_session, ImagesORM
@@ -8,47 +9,59 @@ from schemas import Image, ImageAdd
 class ImageRep:
     @classmethod
     async def get_image(cls, data: ImageAdd) -> Image:
-        async with db_session() as session:
-            image_dict = data.model_dump()
-            if data.name:
+        try:
+            async with db_session() as session:
+                image_dict = data.model_dump()
+                if data.name:
+                    query = sqlalchemy.orm.Query(
+                        ImagesORM, session=session
+                        ).filter(ImagesORM.name == image_dict['name'])
+                    result = await session.execute(query)
+                    ex_image = result.scalars().one_or_none()
+                    return ex_image
                 query = sqlalchemy.orm.Query(
                     ImagesORM, session=session
-                    ).filter(ImagesORM.name == image_dict['name'])
+                    ).filter(ImagesORM.id == data.id)
                 result = await session.execute(query)
                 ex_image = result.scalars().one_or_none()
                 return ex_image
-            query = sqlalchemy.orm.Query(
-                ImagesORM, session=session
-                ).filter(ImagesORM.id == data.id)
-            result = await session.execute(query)
-            ex_image = result.scalars().one_or_none()
-            return ex_image
+        except Exception as error:
+            raise HTTPException(
+                status_code=500,
+                detail=f"DB error man: '{error}'"
+            )
 
     @classmethod
     async def add_image(cls, data: ImageAdd) -> Image:
-        async with db_session() as session:
-            image_dict = data.model_dump()
-            query = sqlalchemy.orm.Query(
-                ImagesORM, session=session
-                ).filter(ImagesORM.title == image_dict['title'])
-            result = await session.execute(query)
-            ex_image = result.scalars().one_or_none()
-            if ex_image:
-                return ex_image
-            name = translit(
-                image_dict['title'], 'ru', reversed=True
-                ).lower().replace(' ', '_')
-            image_dict['name'] = name + '.png'
-            image_data = (generate_image(image_dict['title']))
-            if isinstance(image_data, str):
-                return image_data
-            byte_image, byte_image_size, width, height = image_data
-            image_dict['image'] = byte_image
-            image_dict['byte_image_size'] = byte_image_size
-            image_dict['width'] = width
-            image_dict['height'] = height
-            image = ImagesORM(**image_dict)
-            session.add(image)
-            await session.flush()
-            await session.commit()
-            return image
+        try:
+            async with db_session() as session:
+                image_dict = data.model_dump()
+                query = sqlalchemy.orm.Query(
+                    ImagesORM, session=session
+                    ).filter(ImagesORM.title == image_dict['title'])
+                result = await session.execute(query)
+                ex_image = result.scalars().one_or_none()
+                if ex_image:
+                    return ex_image
+                name = translit(
+                    image_dict['title'], 'ru', reversed=True
+                    ).lower().replace(' ', '_')
+                image_dict['name'] = name + '.png'
+                image_data = (generate_image(image_dict['title']))
+                if isinstance(image_data, str):
+                    return image_data
+                byte_image, byte_image_size, width, height = image_data
+                image_dict['image'] = byte_image
+                image_dict['byte_image_size'] = byte_image_size
+                image_dict['width'] = width
+                image_dict['height'] = height
+                image = ImagesORM(**image_dict)
+                session.add(image)
+                await session.flush()
+                await session.commit()
+                return image
+        except Exception as error:
+            raise HTTPException(
+                status_code=500,
+                detail=f"DB error man: '{error}'"
+            )
